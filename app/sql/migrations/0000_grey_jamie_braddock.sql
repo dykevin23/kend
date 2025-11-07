bcfd9b6a300215ff4eb0c14d5aba4083b0602c4e
kevin <dyk0203@naver.com>
2025-11-07 11:56:47 +0900

    db setting & profile table

---
+++ app/sql/migrations/0000_grey_jamie_braddock.sql
@@ -0,0 +1,21 @@
+CREATE TABLE "follows" (
+	"follower_id" uuid,
+	"following_id" uuid,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "profiles" (
+	"profile_id" uuid PRIMARY KEY NOT NULL,
+	"nickname" text NOT NULL,
+	"username" text NOT NULL,
+	"avatar" text,
+	"introduction" text,
+	"comment" text,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"stats" jsonb
+);
+--> statement-breakpoint
+ALTER TABLE "follows" ADD CONSTRAINT "follows_follower_id_profiles_profile_id_fk" FOREIGN KEY ("follower_id") REFERENCES "public"."profiles"("profile_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "follows" ADD CONSTRAINT "follows_following_id_profiles_profile_id_fk" FOREIGN KEY ("following_id") REFERENCES "public"."profiles"("profile_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "profiles" ADD CONSTRAINT "profiles_profile_id_users_id_fk" FOREIGN KEY ("profile_id") REFERENCES "auth"."users"("id") ON DELETE cascade ON UPDATE no action;
