930bccb65b663eac78781104839bcd0af226b41c
kevin <dyk0203@naver.com>
2025-12-17 11:58:06 +0900

    user to profile trigger update

--- app/sql/triggers/user_to_profile_trigger.sql
+++ app/sql/triggers/user_to_profile_trigger.sql
@@ -12,6 +12,16 @@ begin
             if new.raw_user_meta_data ? 'provider' AND new.raw_user_meta_data ->> 'provider' = 'naver' then
                 insert into public.profiles (profile_id, username, nickname, avatar)
                 values (new.id, new.raw_user_meta_data ->> 'name', new.raw_user_meta_data ->> 'nickname' || substr(md5(random()::text), 1, 5), new.raw_user_meta_data ->> 'profile_image');
+            elsif new.raw_user_meta_data ? 'role' AND new.raw_user_meta_data ->> 'role' = 'seller' then
+                insert into public.profiles (profile_id, nickname, username, role)
+                values (
+                    new.id, 
+                    coalesce(
+                        new.raw_user_meta_data ->> 'nickname',
+                        'seller_' || coalesce(new.raw_user_meta_data ->> 'username',substr(md5(new.id::text), 1, 8))),
+                    new.raw_user_meta_data ->> 'username', 
+                    new.raw_user_meta_data ->> 'role'
+                );
             else 
                 if new.raw_user_meta_data ? 'nickname' AND new.raw_user_meta_data ? 'username' then
                     insert into public.profiles (profile_id, nickname, username)
